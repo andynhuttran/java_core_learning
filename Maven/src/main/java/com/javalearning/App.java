0525bdda23f8eb2c32b9f7dda38e71fdb23286af
sms.cole <sms.cole@cole-lap.net.fpt>
2020-04-04 10:34:46 +0700

    commit maven projects

---
+++ Maven/src/main/java/com/javalearning/App.java
@@ -0,0 +1,13 @@
+package com.javalearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
